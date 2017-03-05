2fa6b16af662a3795f27bc136d5bb014cc6eb0d9
Ashish Verma <ashishrodio@gmail.com>
2017-03-05 23:24:57 +0530

    Added Validations and styles

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -6,6 +6,7 @@ var pomodoroBtn = document.querySelector(".pomodoro");
 var isClicked = true, flag = "interval";
 var id = "";
 var counter = 0, breaks = 0;
+var isActive = false;
 
 intervalUp = document.querySelector(".interval-up");
 intervalDown = document.querySelector(".interval-down");
@@ -50,9 +51,11 @@ function startTime(ty) {
         return;
     } else if (secVal == 0) {
         secVal = 60;
-        min.innerHTML = minVal - 1 == 0 ? "00" : minVal - 1;
+        let newMin = minVal - 1;
+        min.innerHTML = (newMin+"").length < 2 ? "0"+newMin : newMin;
     }
-    sec.innerHTML = secVal - 1 == 0 ? "00" : secVal - 1;
+    let newSec = secVal - 1;
+    sec.innerHTML = (newSec+"").length < 2 ? "0"+newSec : newSec;
     return;
 }
 
@@ -82,11 +85,14 @@ function startPomo() {
  * Interval Adjust
  */
 
+
 intervalUp.addEventListener('click', function () {
+    if(isActive) return;
     countUp(interval);
 });
 
 intervalDown.addEventListener('click', function () {
+    if(isActive) return;
     countDown(interval);
 });
 
@@ -96,12 +102,14 @@ intervalDown.addEventListener('click', function () {
  */
 
 timerUp.addEventListener('click', function () {
+    if(isActive) return;
     countUp(timer);
     min.innerHTML = timer.innerHTML;
     sec.innerHTML = "00";
 });
 
 timerDown.addEventListener('click', function () {
+    if(isActive) return;
     countDown(timer);
     min.innerHTML = timer.innerHTML;
     sec.innerHTML = "00";
@@ -110,9 +118,11 @@ timerDown.addEventListener('click', function () {
 pomodoroBtn.addEventListener('click', function () {
     if (isClicked) {
         id = setInterval(startPomo, 1000);
+        isActive = true;
         isClicked = false;
     } else {
         stopTime();
         isClicked = true;
+        isActive = false;
     }
 });
